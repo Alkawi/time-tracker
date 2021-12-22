5c4a2a552044f16f75a9f48551352919e6590f7a
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-12-22 12:36:35 +0100

    Create simple Form component

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -1,3 +1,61 @@
-export default function Form(): JSX.Element {
-  return <form></form>;
+import { useState } from 'react';
+
+type FormProps = {
+  onSubmit: (
+    description: string,
+    date: Date,
+    startTime: string,
+    endTime: string
+  ) => void;
+};
+
+export default function Form({ onSubmit }: FormProps): JSX.Element {
+  const [description, setDescription] = useState<string>('');
+  const [date, setDate] = useState<Date>(new Date());
+  const [startTime, setStartTime] = useState<string>('');
+  const [endTime, setEndTime] = useState<string>('');
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    onSubmit(description, date, startTime, endTime);
+  }
+  return (
+    <form onSubmit={(event) => handleSubmit(event)}>
+      <label>
+        Description:
+        <textarea
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </label>
+      <label>
+        Date:
+        <input
+          type="date"
+          value={date.toDateString()}
+          onChange={(event) => setDate(new Date(event.target.value))}
+        />
+      </label>
+      <label>
+        Start time:
+        <input
+          type="time"
+          value={startTime}
+          onChange={(event) =>
+            setStartTime(new Date(event.target.value).toLocaleTimeString())
+          }
+        />
+      </label>
+      <label>
+        End time:
+        <input
+          type="time"
+          value={endTime}
+          onChange={(event) =>
+            setEndTime(new Date(event.target.value).toLocaleTimeString())
+          }
+        />
+      </label>
+    </form>
+  );
 }
